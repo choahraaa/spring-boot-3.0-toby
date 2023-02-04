d39992b9efc12f1d59433df59af27338620054c7
choahra <ahar0729@gamil.com>
2023-02-04 19:03:30 +0900

    controller와 service를 분리, null 체크

--- src/main/java/hellospring/tobyspring/HelloController.java
+++ src/main/java/hellospring/tobyspring/HelloController.java
@@ -1,7 +1,12 @@
 package hellospring.tobyspring;
 
+import java.util.Objects;
+
 public class HelloController {
     public String hello(String name){
-        return "Hello " + name;
+        //controller에 주요기능은 유저의 요청사항을 확인해야함
+        SimpleHelloService helloService = new SimpleHelloService(); //service를 사용하여 인사말을 하는 로직을 분리
+
+        return helloService.sayHello(Objects.requireNonNull(name)); //Objects.requireNonNull는 파라미터의 값이 null 인지 체크함
     }
 }
---
+++ src/main/java/hellospring/tobyspring/SimpleHelloService.java
@@ -0,0 +1,7 @@
+package hellospring.tobyspring;
+
+public class SimpleHelloService {
+    String sayHello(String name) {
+        return "Hello" + name;
+    }
+}
