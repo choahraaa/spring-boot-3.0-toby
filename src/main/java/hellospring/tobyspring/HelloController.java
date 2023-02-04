4a9a6ca27940b3d9e0061fb16de2f51c3747ac64
choahra <ahar0729@gamil.com>
2023-02-04 19:29:55 +0900

    helloService dependency add

--- src/main/java/hellospring/tobyspring/HelloController.java
+++ src/main/java/hellospring/tobyspring/HelloController.java
@@ -3,10 +3,13 @@ package hellospring.tobyspring;
 import java.util.Objects;
 
 public class HelloController {
-    public String hello(String name){
-        //controller에 주요기능은 유저의 요청사항을 확인해야함
-        SimpleHelloService helloService = new SimpleHelloService(); //service를 사용하여 인사말을 하는 로직을 분리
+    private final HelloService helloService;
 
+    public HelloController(HelloService helloService) {
+        this.helloService = helloService;
+    }
+
+    public String hello(String name){
         return helloService.sayHello(Objects.requireNonNull(name)); //Objects.requireNonNull는 파라미터의 값이 null 인지 체크함
     }
 }
