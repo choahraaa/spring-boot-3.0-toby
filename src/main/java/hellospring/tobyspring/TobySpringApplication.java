f722934c3be80a5f98f2d95348ca3a0f091a6c2e
choahra <ahar0729@gamil.com>
2023-02-04 23:09:16 +0900

    기존 springBoot안에서의 run으로 수정

--- src/main/java/hellospring/tobyspring/TobySpringApplication.java
+++ src/main/java/hellospring/tobyspring/TobySpringApplication.java
@@ -1,31 +1,12 @@
 package hellospring.tobyspring;
 
 
-import org.apache.catalina.startup.Tomcat;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.server.WebServer;
-import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.stereotype.Component;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.context.support.GenericWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
-
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
-import static hellospring.tobyspring.MySpringApplication.run;
 
 @Configuration
 @ComponentScan //component annotation이 붙은 class를 bean으로 등록해달라는 annotation
@@ -41,7 +22,7 @@ public class TobySpringApplication {
     }
 
     public static void main(String[] args) {
-        run(TobySpringApplication.class, args);
+        SpringApplication.run(TobySpringApplication.class, args);
     }
 
 
