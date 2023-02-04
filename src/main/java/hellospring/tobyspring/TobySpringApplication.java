66c371556f63aa0417dd46de57bfdead685c26ba
choahra <ahar0729@gamil.com>
2023-02-04 23:05:05 +0900

    외부 method를 실행하는 코드

--- src/main/java/hellospring/tobyspring/TobySpringApplication.java
+++ src/main/java/hellospring/tobyspring/TobySpringApplication.java
@@ -25,6 +25,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import static hellospring.tobyspring.MySpringApplication.run;
+
 @Configuration
 @ComponentScan //component annotation이 붙은 class를 bean으로 등록해달라는 annotation
 public class TobySpringApplication {
@@ -39,27 +41,9 @@ public class TobySpringApplication {
     }
 
     public static void main(String[] args) {
-        //springcontainer 생성
-        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext(){
-          //확장하고 싶은 기능들
-            @Override
-            protected void onRefresh() { //dispatcherServlet 초기화하는 중에 작업해야 할 것들이 있을 때 사용
-                super.onRefresh();
-
-                //servletContainer를 코드로 실행하면서 servlet을 등록
-                TomcatServletWebServerFactory serverFactory = this.getBean(TomcatServletWebServerFactory.class);
-                DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
-                dispatcherServlet.setApplicationContext(this);
-
-                WebServer webServer = serverFactory.getWebServer(servletContext -> {
-                    servletContext.addServlet("dispatcher", dispatcherServlet).addMapping("/*");
-                });
-                webServer.start(); //tomcat servlet container start
-            }
-        };
-        applicationContext.register(TobySpringApplication.class);
-        //simpleHelloService bean을 구현한 것이 있는지 찾아보고 구현한 helloController 생성자로 넣어주는데 어떤 걸 먼저 등록해줘도 됨(순서는 상관없음)
-        applicationContext.refresh(); //container 초기화(bean object 생성)
+        run(TobySpringApplication.class, args);
     }
+
+
 
 }
