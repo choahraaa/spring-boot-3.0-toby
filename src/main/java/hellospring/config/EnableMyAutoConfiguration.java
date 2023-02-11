7611d9032c8a4bdb9a45d8e58a0f3fba09afc9b3
choahra <ahar0729@gamil.com>
2023-02-11 22:28:34 +0900

    ImportSelector를 사용해서 config파일을 설정

--- src/main/java/hellospring/config/EnableMyAutoConfiguration.java
+++ src/main/java/hellospring/config/EnableMyAutoConfiguration.java
@@ -9,6 +9,6 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
-@Import(MyAutoConfigImportSelector.class) //config 설정파일을 ImportSelector를 통해서 자유자재로 추가할 수 있음
+@Import(MyAutoConfigImportSelector.class) //config 설정파일을 ImportSelector를 통해서 자유자재로 추가할 수 있
 public @interface EnableMyAutoConfiguration {
 }
